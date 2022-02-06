4a935c5c0b284618d71defcdafb05010b46cffb2
P <19521998@gm.uit.edu.vn>
2022-02-06 15:57:34 +0700

    change model

--- Chit-ChatTests/Model_Test.swift
+++ Chit-ChatTests/Model_Test.swift
@@ -9,25 +9,33 @@ import XCTest
 @testable import Chit_Chat
 
 class Model_Test: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testingModel() throws {
-        try testUser()
-        try testConversation()
-        try testUserNetwork()
-    }
+    
+    //    func testingModel() throws {
+    //        try testUser()
+    //        try testConversation()
+    //        try testUserNetwork()
+    //    }
     
     func testUser() throws {
         let latestMessage = LatestMessage(date: Date(), text: "Hello World", isRead: false)
         
         let conversations = MessagesCollection(id: "fir5tM3ss4g35", name: "Doctor", otherUserEmail: "yds@gm.yds.edu.vn", latestMessage: latestMessage)
+        
+        let node = UserNode(id: "hash123",
+                            firstName: "Khoi",
+                            lastName: "Le",
+                            bio: "This is my bio",
+                            email: "uit@gm.uit.edu.vn",
+                            dob: Date(),
+                            isMale: true)
         
         let instance = User(id: "hash123",
                             firstName: "Khoi",
@@ -37,6 +45,7 @@ class Model_Test: XCTestCase {
                             password: "SwiftyHash",
                             dob: Date(),
                             isMale: true,
+                            friendList: [node],
                             conversations: [conversations])
         
         XCTAssertNotNil(instance, "Sorry, user model test case failed.")
@@ -44,22 +53,23 @@ class Model_Test: XCTestCase {
     }
     
     func testConversation() throws {
-        let conversation = Conversation(id: "fir5tM3ss4g35", type: "text", content: "Hello World", sender_email: "yds@gm.yds.edu.vn", name: "Doctor")
+        let conversation = MessageOfConversation(id: "fir5tM3ss4g35", type: "text", content: "Hello World", sender_email: "yds@gm.yds.edu.vn", name: "Doctor")
         let instance = Conversations(messages: [conversation])
         
         XCTAssertNotNil(instance, "Sorry, conversation model test case failed.")
     }
     
     func testUserNetwork() throws {
-        let instance = UserNetwork(id: "hash123",
-                                   firstName: "Khoi",
-                                   lastName: "Le",
-                                   bio: "This is my bio",
-                                   email: "uit@gm.uit.edu.vn",
-                                   dob: Date(),
-                                   isMale: true)
+        let node = UserNode(id: "hash123",
+                            firstName: "Khoi",
+                            lastName: "Le",
+                            bio: "This is my bio",
+                            email: "uit@gm.uit.edu.vn",
+                            dob: Date(),
+                            isMale: true)
+        let instance = UserNetwork(listUser: [node])
         
         XCTAssertNotNil(instance, "Sorry, user network model test case failed.")
     }
-
+    
 }
