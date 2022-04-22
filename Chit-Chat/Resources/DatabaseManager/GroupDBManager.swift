79109832d3c8e63ccb5ba1c0e1949a28816480e7
KhoiLe <Khoilecontact@gmail.com>
2022-04-22 22:08:27 +0700

    init new function

---
+++ Chit-Chat/Resources/DatabaseManager/GroupDBManager.swift
@@ -0,0 +1,18 @@
+//
+//  GroupDBManager.swift
+//  Chit-Chat
+//
+//  Created by KhoiLe on 21/04/2022.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseAuth
+import MessageKit
+import CoreLocation
+
+// MARK: Grouping and group
+
+extension DatabaseManager {
+    
+}
